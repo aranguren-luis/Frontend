56af4ff23d62b895d2f54e48d7bdf1f8ce5fe8ce
crislam93 <39211349+crislam93@users.noreply.github.com>
2020-04-28 18:31:49 +0200

    Using W3-CSS Classes, querySelector for a BG-Color-Changer

---
+++ JS/bgchange.js
@@ -0,0 +1,28 @@
+"use strict"
+
+function purple()
+                {
+                    let purple                   = document.querySelector(".cuerpo");
+                    let title                    = document.querySelector(".titulo");
+                    purple.style.backgroundColor = "purple";
+                    title.style.color = "white";
+                    console.log("yes!");
+                };
+
+function indigo()
+                {
+                    let indigo                   = document.querySelector(".cuerpo");
+                    let title                    = document.querySelector(".titulo");
+                    indigo.style.backgroundColor = "indigo";
+                    title.style.color            = "greenyellow";
+                    console.log("yes!");
+                };
+
+function orange()
+                {
+                    let orange                   = document.querySelector(".cuerpo");
+                    let title                    = document.querySelector(".titulo"); 
+                    orange.style.backgroundColor = "orange";
+                    title.style.color            = "blue";
+                    console.log("yes!");
+                };
