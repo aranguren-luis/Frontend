f1a2dcfd4d79fb9e838da76a2ed80cb1983528be
crislam93 <39211349+crislam93@users.noreply.github.com>
2020-04-30 16:29:13 +0200

    ...

--- JS/azar.js
+++ JS/azar.js
@@ -1,63 +1,24 @@
 "use strict"
 
-var char = String.fromCharCode;
-
-//https://unicode-table.com/en/#control-character < 
-
-//Función para obtener minúsculas
-function lowerCase()
-    {
-        let number   = Math.floor( Math.random() * 26 ) + 97; // 97: consultar link de arriba
-        var lowcase  = String.fromCharCode( number );
-        return lowcase;
-    };
-
-//Función para obtener letras mayúsculas
-function upperCase()
-    {
-        let number    = Math.floor( Math.random() * 26 ) + 65; // 65: consultar link de arriba
-        var uppercase = String.fromCharCode ( number );
-        return uppercase;
-    };
-
-//Función para obtener número aleatorio
-function randomNumber()
-    {
-        var random = String.fromCharCode( Math.floor( Math.random() * 10 ) * 48 ); // 48: consultar link de arriba
-        return random;
-    };
-
-//Función para obtener símbolo
-function symbolo()
-    {
-        var simbolo = "!@#$%&*";
-        return simbolo[ Math.floor( Math.random() * simbolo.length ) ];
-    };
-
-
 const regxs = 
     {
-        "lower": /^[a-z0-9 ]+$/,
-        "upper": /^[A-Z0-9 ]+$/
+        "lower": /^[ a-z ]+$/,
+        "upper": /^[ A-Z ]+$/,
+        "upperLower": /^[ 0-9 ]+$/
     }
 
 
 
 document.querySelector( ".userInput" ).onkeyup = function()
     {
-        var tecleo           = document.querySelector( ".userInput" ).onkeyup; 
         const cardHeader     = document.querySelector( "#cardHeader" );
         var   userInput      = document.querySelector( ".userInput" ).value;
         cardHeader.innerHTML = "<span class = 'text-success'>" + userInput.length + "</span>"; 
 
         if( regxs.lower.test( userInput ) )
             {
-                const minuscula     = document.querySelector("#minini");   
-                minuscula.innerHTML = "<input type='checkbox' checked>";  
-        
-            };
-
+                const minuscula     = document.querySelector( "#minini" );   
+                minuscula.innerHTML = "<input type='checkbox' checked>";
+            } 
     };
-
-
 
