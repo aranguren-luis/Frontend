a518d1cb7f5ec4b860614b47f168796d2fbb7d33
crislam93 <39211349+crislam93@users.noreply.github.com>
2020-05-02 16:11:29 +0200

    API pokeon name, number & img

--- JS/pokeAPI.js
+++ JS/pokeAPI.js
@@ -3,13 +3,15 @@
 function pokemon()
     {
         const userInput = document.querySelector( ".MainInput" ).value;
-        const url       = "https://pokeapi.co/api/v2/pokemon/?offset=" + userInput + "&limit=1";
-        
+        const url       = "https://pokeapi.co/api/v2/pokemon/" + userInput;
+
         fetch( url )
         .then( response => response.json() )
         .then( data => 
             {
-                console.log( data.results[0].name );
+                let pokeName       = document.querySelector( ".pokeInfo" );
+                pokeName.innerHTML = `<p>${data.name} #${data.order}</p>`;
+                pokeName.innerHTML += `<img src="${data.sprites.front_default}"/>`;
             })
         .catch( err => console.log( err ) );
     };
