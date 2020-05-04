24726ff06f0e9253b26c8aa583d9c4273de57feb
crislam93 <39211349+crislam93@users.noreply.github.com>
2020-05-04 12:27:53 +0200

    Objetos: Frutas

---
+++ JS/practice.js
@@ -0,0 +1,27 @@
+"use strict"
+
+var manzana = 
+    {
+        color      : "rojo",
+        tamaño     : 10.3,
+        peso       : 50 + " gramos",
+        comestible : true,
+        tipo       : "fruta",
+        rueda      : function(){ return "puedo rodar y estar parada" },
+        colores    : [ "rojo", "verde", "amarillo", "ambar" ]
+    };
+
+var fruta        = new Object();
+fruta.name       = ["Banana", "Platano", "Cambur"];
+fruta.color      = "verde";
+fruta.tamaño     = 15.7;
+fruta.peso       = 100 + "gramos";
+fruta.comestible = true; 
+fruta.tipo       = "fruta";
+fruta.nada       = function() { return "no hago nada" };
+fruta.colores    = [ "amarillo", "verde", "Blue Java", "Rouges" ];
+console.log(fruta);
+
+var verFruta = document.querySelector( ".titulo" );
+verFruta.innerHTML = `<br>Fruta:       ${fruta.name}
+                      <br>Colores:     ${fruta.colores}`;
