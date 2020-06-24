1f1bd705ca661eca821cc6366b6d334f4455fb13
crislam93 <39211349+crislam93@users.noreply.github.com>
2020-06-24 18:03:47 +0200

    ...

---
+++ Vue.JS/Project_0/index.js
@@ -0,0 +1,8 @@
+var app3 = new Vue
+                ({
+                    el   : "#app-3",
+                    data : 
+                        {
+                            seen: true
+                        } 
+                })
---
+++ Vue.JS/Project_1/index.js
@@ -0,0 +1,13 @@
+var app4 = new Vue  
+        ({
+            el   : "#app-4",
+            data : 
+                    {
+                        lista : 
+                                [
+                                    { text : "Aprender Javascript" },
+                                    { text : "Aprender Vue" },
+                                    { text : "Construir Algo Increíble" }
+                                ]
+                        }
+            })
